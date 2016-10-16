9ea24808a739fc68e15053ba57b7c9cdf62f438d
Sergio Garcia <sergio@garciparedes.me>
2016-10-16 18:09:19 +0200

    Updated gitignore

---
+++ src/weka/WekaMultiLayerPerceptron.java
@@ -0,0 +1,12 @@
+package weka;
+
+import weka.core.Instances;
+
+/**
+ * Created by garciparedes on 16/10/2016.
+ */
+public class WekaMultiLayerPerceptron {
+    public static void main(String[] args) {
+
+    }
+}
